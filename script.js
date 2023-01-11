5d7196b9d0dee9eed65e877d2dd36ba0df1e571e
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-01-11 16:45:12 -0300

    cartas giram onclick

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 const numerocartas = prompt("Com quantas cartas quer jogar? (4 a 14, numeros pares");
 if(numerocartas == 4 || numerocartas == 6 || numerocartas == 8 || numerocartas == 10 || numerocartas == 12 || numerocartas == 14){
 let cont = 4;
+function girar(carta){
+    carta.querySelector(".back-face").classList.toggle("girar-back-face");
+    carta.querySelector(".front-face").classList.toggle("girar-front-face");
+}
 while(cont <= numerocartas)
 {
 cont++;
